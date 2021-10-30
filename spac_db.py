73836264fcad7756e04c6e9435a7416239b151b6
Payas Parab <payas.parab@ninepointtwo.com>
2021-10-30 11:18:51 -0700

    shelled out code to merge in datasets and key DB update

--- spac_db.py
+++ spac_db.py
@@ -1,4 +1,38 @@
-def generate_spac_db(): 
-    '''
-    Pulls from spac_data csvs to consolidate 
-    '''
+import requests
+import pandas as pd
+import os
+import selenium
+
+
+from ib_insync.util import df as df_util
+from ib_insync import util
+from ib_insync import IB
+from ib_insync import Contract, Stock, MarketOrder, Future
+
+
+
+class SPAC_DB:
+    def __init__(self):
+        
+        ''' 
+        Pull in raw data that makes up
+        '''
+        self.raw_data_root = os.path.dirname(os.path.abspath(__file__)) + '\\spac_data\\'
+        self.unmerged = pd.read_csv(self.raw_data_root + 'unannounced_merger_spacs.csv')
+        self.pending = pd.read_csv(self.raw_data_root + 'pending_merger_spacs.csv')
+        self.public_spacs = pd.read_csv(self.raw_data_root + 'public_merged_spacs.csv')
+
+        
+    def update_csvs(self):
+        '''
+        Method that pulls web pages links and updates csv files in the spac_data root
+        '''
+        unpending_merger_link = 'https://stockmarketmba.com/listofspacswithoutapendingmerger.php'
+        pending_merger_link = 'https://stockmarketmba.com/pendingspacmergers.php'
+        publically_listed_spacs = 'https://stockmarketmba.com/listofcompaniesthathavemergedwithaspac.php'
+
+        
+    def create_returns_series(self): 
+        '''
+        
+        '''
