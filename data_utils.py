b6237a2fc4aa73e4ebe6bd765f59c6036ea33105
Payas Parab <payas.parab@ninepointtwo.com>
2021-10-31 10:29:55 -0700

    data_utils: created a standardization of the data cleaners

---
+++ data_utils.py
@@ -0,0 +1,21 @@
+'''
+Data Standardization Tools for Consistency
+'''
+
+def df_cleaner(df):
+    
+
+    def pd_name_cleaner(ugly_str):
+        ''' Make any string pandas friendly '''
+        import re
+        ugly_str = ugly_str.lower() # Lowercase 
+        ugly_str = ugly_str.replace(' ', '_') # No spaces
+        ugly_str = re.sub(r'\W+', '', ugly_str) # Alpha numeric only
+        ugly_str = ugly_str[:100] # Shorten
+        return ugly_str
+
+
+    df.columns = list(map(pd_name_cleaner, list(df.columns)))
+
+    return df 
+    
--- spac_db.py
+++ spac_db.py
@@ -1,11 +1,9 @@
 import requests
 import pandas as pd
 import os
-import selenium
+import re 
 
-
-
-
+from data_utils import pd_name_cleaner
 
 class SPAC_DB:
     def __init__(self):
@@ -15,10 +13,23 @@ class SPAC_DB:
         '''
         self.raw_data_root = os.path.dirname(os.path.abspath(__file__)) + '\\data\\'
         self.unmerged = pd.read_csv(self.raw_data_root + 'spac_data\\unannounced_merger_spacs.csv')
-        self.pending = pd.read_csv(self.raw_data_root + 'spac_data\\pending_merger_spacs.csv')
-        self.public_spacs = pd.read_csv(self.raw_data_root + 'spac_data\\public_merged_spacs.csv')
-        self.price_data = pd.read_csv(self.raw_data_root + 'price_data.csv').set_index(['date', 'ticker']).sort_index()
+        self.unmerged.columns = list(self.unmerged.columns).apply(pd_name_cleaner)
         
+        self.pending = pd.read_csv(self.raw_data_root + 'spac_data\\pending_merger_spacs.csv')
+        self.pending['']
+        
+        merger_proposal_data = 1 #\d+\/\d+\/\d+
+
+
+
+
+        self.public_spacs = pd.read_csv(self.raw_data_root + 'spac_data\\public_merged_spacs.csv')
+        
+        
+        self.price_data = pd.read_csv(self.raw_data_root + 'price_data.csv').set_index(['date', 'ticker']).sort_index()
+        self.price_data['rets'] = self.price_data.unstack().pct_change(fill_method=None).stack()
+
+
     def update_csvs(self):
         '''
         Method that pulls web pages links and updates csv files in the spac_data root
