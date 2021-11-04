b45014117d2ce9e14e9837c23c27b24ad4ddfba1
Payas Parab <payas.parab@ninepointtwo.com>
2021-11-04 16:50:12 -0700

    spac_research: done playing around with the data and have an idea of how to move forward with portfolio construction

---
+++ portfolio_construction.py
@@ -0,0 +1,12 @@
+
+def construct_naive_portfolio(weighted_by, freq, spac_types):
+    '''
+    Create a portfolio per
+
+    weighted_by : str : 
+    freq : int : portfolio rebalancing period by trading days
+    type : lst(str) : contains types of SPACs to include
+
+    
+    '''
+    pass
